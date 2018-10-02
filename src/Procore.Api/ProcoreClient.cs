8682cff4eafaf753d598937a67c1c3ecc85335cf
Adam Curtis <adamcurtisvt@gmail.com>
2018-10-02 10:34:42 -0400

    Add Procore client (#2)

---
+++ src/Procore.Api/ProcoreClient.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("Procore.Api.Tests")]
+namespace Procore.Api
+{
+    /// <summary>
+    ///     Represents a client that encapsulate other clients which interact with the Procore API.
+    /// </summary>
+    public class ProcoreClient
+    {
+        //---------------------------------------------------------------------
+        // Constructor - Public
+        //---------------------------------------------------------------------
+
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="ProcoreClient" /> class.
+        /// </summary>
+        /// <param name="token">OAuth 2.0 token obtained from Procore.</param>
+        /// <param name="httpClient"><see cref="HttpClient" /> used to perform the requests.</param>
+        /// <exception cref="ArgumentNullException" />
+        public ProcoreClient(string token, HttpClient httpClient = null)
+        {
+            // Determine if the token is null.
+            if (string.IsNullOrWhiteSpace(token))
+            {
+                throw new ArgumentNullException(nameof(token));
+            }
+
+            // Determine if the HTTP client is null.
+            httpClient = httpClient ?? new HttpClient {BaseAddress = new Uri("https://app.procore.com")};
+            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+        }
+    }
+}
---
+++ test/Procore.Api.Tests/ProcoreClientTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Xunit;
+
+namespace Procore.Api.Tests
+{
+    public class ProcoreClientTests
+    {
+        //---------------------------------------------------------------------
+        // Constructor
+        //---------------------------------------------------------------------
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        public void Constructor_InvalidToken_ThrowArgumentNullException(string token)
+        {
+            Assert.Throws<ArgumentNullException>(() => new ProcoreClient(token));
+        }
+    }
+}
